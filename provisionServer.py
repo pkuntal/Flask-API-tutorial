016a0097246434bd489443f782583dabbee6b722
Kuntal <pkuntal@amd.com>
2018-04-25 11:45:49 +0530

    db import issue

--- provisionServer.py
+++ provisionServer.py
@@ -30,9 +30,6 @@ app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 #customizing JWT auth response
 #this will return only access_code
