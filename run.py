4cb2f87e76ca697abe4368bb50d3b697717b96b7
Kuntal <pkuntal@amd.com>
2018-04-25 11:48:16 +0530

    db import issue

---
+++ run.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 25 11:42:48 2018
+
+@author: pkuntal
+"""
+
+from provisionServer import app
+from db import db
+db.init(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
